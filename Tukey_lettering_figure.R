a4deca1164f7699e603c0ba96707d27de8941fe1
fbelbin <67378915+fbelbin@users.noreply.github.com>
2020-06-24 10:39:59 -0400

    Update Tukey_lettering_figure.R

--- Tukey_lettering_figure.R
+++ Tukey_lettering_figure.R
@@ -9,28 +9,28 @@ dat1$Genotype <- factor(dat1$Genotype, levels=c("A1", "U", "8", "9", "10", "11")
 
 #######################################
 
-#CCA1
+#G1
 
-# filter for CCA1
-g_CCA1 <- dat1 %>% filter(Gene == "CCA1") 
+# filter for Gene1
+g_G1 <- dat1 %>% filter(Gene == "Gene1") 
 
 #to sort x axis by genotype in my chosen order
-g_CCA1$Genotype <- factor(g_CCA1$Genotype, levels=c("A1", "U", "8", "9", "10", "11"))
+g_G1$Genotype <- factor(g_G1$Genotype, levels=c("A1", "U", "8", "9", "10", "11"))
 
 
 # ANOVA
-model <- lm( g_CCA1$Ct ~ g_CCA1$Genotype )
-CCA1ANOVA <- aov(model)
-CCA1ANOVA
+model <- lm( g_G1$Ct ~ g_G1$Genotype )
+G1ANOVA <- aov(model)
+G1ANOVA
 
 # Tukey test
-CCA1TUKEY <- TukeyHSD(x=CCA1ANOVA, 'g_CCA1$Genotype', conf.level=0.95)
-CCA1TUKEY
+G1TUKEY <- TukeyHSD(x=G1ANOVA, 'g_G1$Genotype', conf.level=0.95)
+G1TUKEY
 
 # Group the treatments that are not different each other together (?)-- Taken from https://www.r-graph-gallery.com/84-tukey-test
-CCA1generate_label_df <- function(CCA1TUKEY, variable){
+G1generate_label_df <- function(G1TUKEY, variable){
   # Extract labels and factor levels from Tukey post-hoc 
-  Tukey.levels <- CCA1TUKEY[[variable]][,4]
+  Tukey.levels <- G1TUKEY[[variable]][,4]
   Tukey.labels <- data.frame(multcompLetters(Tukey.levels)['Letters'])
   #Putting labels in same order as in the boxplot (?) 
   Tukey.labels$Genotype=rownames(Tukey.labels)
@@ -39,25 +39,25 @@ CCA1generate_label_df <- function(CCA1TUKEY, variable){
 }
 
 # Apply the function on my dataset --> making dataframe with lettering
-CCA1LABELS <- CCA1generate_label_df(CCA1TUKEY , "g_CCA1$Genotype")
+G1LABELS <- G1generate_label_df(G1TUKEY , "g_G1$Genotype")
 
 # re-order to match re-ordering of x (genotype)
-CCA1LABELS <- CCA1LABELS[order(levels=c("A1", "U", "8", "9", "10", "11")),]
+G1LABELS <- G1LABELS[order(levels=c("A1", "U", "8", "9", "10", "11")),]
 
 
 my_y_title <- expression(paste("Transcript abundance relative to ", italic("IPP2")))
 
 # Draw the boxplot
 
-PCCA1 <- g_CCA1 %>% filter(Gene == "CCA1") %>% ggplot(aes(Genotype, Ct, fill = Genotype)) +
+PG1 <- g_G1 %>% filter(Gene == "G1") %>% ggplot(aes(Genotype, Ct, fill = Genotype)) +
   geom_boxplot(outlier.color = NA, alpha = 0.5) +
   geom_point(aes(color = Genotype), alpha = 0.8) + 
   labs(x= "Genotype", y=my_y_title) + #axis labels
   theme_minimal() + 
   scale_fill_brewer(palette="Set2") + 
   scale_color_brewer(palette = "Set2") + 
-  ggtitle("CCA1") + 
+  ggtitle("G1") + 
   theme(plot.title = element_text(face = "bold.italic", hjust = 0.9, size = 14)) + #graph title
-  geom_text(data=CCA1LABELS, aes(x=Genotype, y = c(1.25, 0.9, 0.95, 0.7, 0.55, 0.6), label = Letters), size=4)  #adding in lettering 
- 
-PCCA1 
+  geom_text(data=G1LABELS, aes(x=Genotype, y = c(1.25, 0.9, 0.95, 0.7, 0.55, 0.6), label = Letters), size=4)  #adding in lettering 
+
+PG1 
